71f51b8795cdf181bd32f31e797afcfc9d3ae710
jaydenrobinette416 <jaydenrobinette416@gmail.com>
2026-06-06 14:26:52 -0400

    Enable DELETE method for item removal
    Added support for DELETE method to remove an item by ID.

--- api/unit-info.js
+++ api/unit-info.js
@@ -18,7 +18,7 @@ async function connectToDatabase() {
 
 module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") return res.status(200).end();
@@ -128,6 +128,25 @@ module.exports = async function handler(req, res) {
       });
     }
 
+if (req.method === "DELETE") {
+  const id = req.query.id;
+
+  if (!id) {
+    return res.status(400).json({
+      ok: false,
+      error: "Missing id"
+    });
+  }
+
+  await collection.deleteOne({
+    _id: new ObjectId(id)
+  });
+
+  return res.status(200).json({
+    ok: true
+  });
+}
+    
     return res.status(405).json({
       ok: false,
       error: "Method not allowed"
